db715b042dae9ba0d6b55a2da65c2dba19fded57
Dylan <packe089@morris.umn.edu>
2024-09-24 11:11:53 -0500

    fixed error sum for check_whitespace.c and finsihed check_whitespace_test

--- check_whitespace.c
+++ check_whitespace.c
@@ -6,42 +6,36 @@
  * Strips spaces from both the front and back of a string,
  * leaving any internal spaces alone.
  */
-char const *strip(char const *str) {
+char* strip(char const *str) {
   int size = strlen(str);
 
   // This counts the number of leading and trailing spaces
   // so we can figure out how big the result array should be.
-  int num_spaces = 0;
   int first_non_space = 0;
   while (first_non_space < size && str[first_non_space] == ' ') {
-    ++num_spaces;
     ++first_non_space;
   }
 
-  int last_non_space = size-1;
+  int last_non_space = size - 1;
   while (last_non_space >= 0 && str[last_non_space] == ' ') {
-    ++num_spaces;
     --last_non_space;
   }
 
-  // If num_spaces >= size then that means that the string
-  // consisted of nothing but spaces, so we'll return the
-  // empty string.
-  if (num_spaces >= size) {
-    return "";
+  // If the string consists of nothing but spaces, return an empty string.
+  if (first_non_space > last_non_space) {
+    char* result = (char*) calloc(1, sizeof(char));
+    result[0] = '\0';
+    return result;
   }
 
   // Allocate a slot for all the "saved" characters
   // plus one extra for the null terminator.
-  char* result = (char*) calloc(size-num_spaces+1, sizeof(char));
+  int new_size = last_non_space - first_non_space + 1;
+  char* result = (char*) calloc(new_size + 1, sizeof(char));
 
   // Copy in the "saved" characters.
-  int i;
-  for (i = first_non_space; i <= last_non_space; ++i) {
-    result[i-first_non_space] = str[i];
-  }
-  // Place the null terminator at the end of the result string.
-  result[i-first_non_space] = '\0';
+  strncpy(result, str + first_non_space, new_size);
+  result[new_size] = '\0';
 
   return result;
 }
@@ -53,7 +47,7 @@ char const *strip(char const *str) {
 int is_clean(char const *str) {
   // We check if it's clean by calling strip and seeing if the
   // result is the same as the original string.
-  char const *cleaned = strip(str);
+  char* cleaned = strip(str);
 
   // strcmp compares two strings, returning a negative value if
   // the first is less than the second (in alphabetical order),
@@ -61,10 +55,8 @@ int is_clean(char const *str) {
   // greater than the second.
   int result = strcmp(str, cleaned);
 
-// Free the memory allocated by strip if it is not the empty string
-  if (cleaned != str && cleaned[0] != '\0') {
-    free((void*)cleaned);
-  }
+  // Free the memory allocated by strip
+  free(cleaned);
 
   return result == 0;
 }
